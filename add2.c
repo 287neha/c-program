e424d7395fabe8bfb47d5d26bb97c1646bef47fc
287neha <46436667+287neha@users.noreply.github.com>
2019-03-06 18:05:10 +0530

    Create add2.c

---
+++ add2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int input(int *a,int *b)
+{
+ printf("enter the numbers");
+ scanf("%d%d",a,b);
+}
+int compute(int a,int b,int *sum)
+{
+ *sum=a+b;
+}
+void output(int sum)
+{
+ printf("sum is %d",sum);
+}
+int main()
+{
+ int a,b,sum;
+ input(&a,&b);
+ compute(a,b,&sum);
+ output(sum);
+}
+ 
