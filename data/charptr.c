a4d340cee99c7b26bc261951b6ffd0be6a444f0e
287neha <46436667+287neha@users.noreply.github.com>
2019-06-19 11:46:05 +0530

    Create charptr.c

---
+++ data/charptr.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void string(int n,char *s[])
+{
+ char a[20];
+ for(int i=0;i<n;i++)
+ {
+  printf("enter the word : ");
+  scanf("%s",a);
+  s[i]=malloc(strlen(a)+1);
+  strcpy(s[i],a);
+ }
+}
+int compare(const void* p,const void* q)
+{
+ return strcmp(*(const char**) p,*(const char**) q);
+} 
+void output(int n,char *s[45])
+{
+ printf("the sorted string is: ");
+ for(int i=0;i<n;i++)
+ {
+  printf("%s  ",s[i]);
+ }
+}
+int main()
+{
+ int n;
+ printf("enter the number of words : ");
+ scanf("%d",&n);
+ char *s[n];
+ string(n,s);
+ qsort(s,n,sizeof(char*),compare);
+ output(n,s);
+ return 0;
+}
